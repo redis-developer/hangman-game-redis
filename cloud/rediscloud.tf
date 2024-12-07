ef0a729a51bb0e3abf8c9592a66cf6f6dcb5ff55
Ricardo Ferreira <riferrei@riferrei.com>
2024-12-07 11:10:09 -0500

    Descriptive name for Redis Cloud plan

--- cloud/rediscloud.tf
+++ cloud/rediscloud.tf
@@ -10,19 +10,19 @@ terraform {
 provider "rediscloud" {
 }
 
-data "rediscloud_essentials_plan" "rediscloud_essentials_plan" {
+data "rediscloud_essentials_plan" "rediscloud_essentials_plan_30mb" {
   name           = "30MB"
   cloud_provider = "AWS"
   region         = "us-east-1"
 }
 
-resource "rediscloud_essentials_subscription" "hangman_game_redis" {
-  name    = local.unique_name
-  plan_id = data.rediscloud_essentials_plan.rediscloud_essentials_plan.id
+resource "rediscloud_essentials_subscription" "rediscloud_essentials_plan_30mb" {
+  name    = "rediscloud_essentials_plan_30mb"
+  plan_id = data.rediscloud_essentials_plan.rediscloud_essentials_plan_30mb.id
 }
 
 resource "rediscloud_essentials_database" "hangman_game_redis" {
-  subscription_id     = rediscloud_essentials_subscription.hangman_game_redis.id
+  subscription_id     = rediscloud_essentials_subscription.rediscloud_essentials_plan_30mb.id
   name                = local.unique_name
   enable_default_user = true
   password            = "See_H0w_Fast_Fee1s"
